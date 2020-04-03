5f751d1ea973a254c3368501d16ebd0ced2aed63
OGLinuk <daniel-l-black@protonmail.com>
2020-04-03 17:38:25 +1300

    Added create page functionality

--- ui/handlers.go
+++ ui/handlers.go
@@ -4,22 +4,20 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	cpb "github.com/OGLinuk/micro-todos/services/create/proto"
+	rpb "github.com/OGLinuk/micro-todos/services/retrieve/proto"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	t := []*cpb.Task{}
+	t := []*rpb.Task{}
 
-	task := &cpb.Task{
-		Name:        "test",
-		Description: "this is a desc",
-		Priority:    int32(3),
-	}
-
-	req := &cpb.CreateRequest{Token: "test", Task: task}
-	if resp, err := createClient.Create(context.Background(), req); err == nil {
-		t = append(t, resp.Task)
+	req := &rpb.RetrieveAllRequest{Token: "test"}
+	if resp, err := retrClient.RetrieveAll(context.Background(), req); err == nil {
+		for _, cTask := range resp.Tasks {
+			t = append(t, cTask)
+		}
 	} else {
 		http.Error(w, fmt.Sprintf("handlers.go::index::createClient.Create::ERROR: %s", err.Error()), 500)
 	}
@@ -31,6 +29,28 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		desc := r.FormValue("desc")
+		prio, err := strconv.ParseInt(r.FormValue("priority"), 10, 32)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("handlers.go::create::strconv.ParseInt::ERROR: %s", err.Error()), 500)
+		}
+
+		t := &cpb.Task{
+			Name:        name,
+			Description: desc,
+			Priority:    int32(prio),
+		}
+
+		_, err = createClient.Create(context.Background(), &cpb.CreateRequest{Task: t})
+		if err != nil {
+			http.Error(w, fmt.Sprintf("handlers.go::create::createClient.Create::ERROR: %s", err.Error()), 500)
+		}
+
+		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+	}
+
 	err := tpl.ExecuteTemplate(w, "create.html", nil)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("handlers.go::create::tpl.ExecuteTemplate::ERROR: %s", err.Error()), 500)
